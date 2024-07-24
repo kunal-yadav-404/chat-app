4fea10c1d94ec7b94cebb8ce4d6feb105f35ac4e
kunal-yadav-404 <yadavkunal780@gmail.com>
2024-07-24 23:26:32 +0530

    dynamic no of user

--- server.js
+++ server.js
@@ -11,47 +11,66 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 let users = [];
+let userLimit = 0;
+let userLimitSet = false;
 
 io.on("connection", (socket) => {
-  console.log("New user connected");
-
-  if (users.length < 2) {
-    users.push(socket);
-    socket.emit("user connected", {
-      msg: "You are connected. Waiting for another user...",
-    });
-
-    if (users.length === 2) {
-      users.forEach((user, index) => {
-        user.emit("user connected", {
-          msg: `User ${index + 1} connected. You can now chat.`,
-        });
-      });
-    }
-
-    socket.on("chat message", (msg) => {
-      users.forEach((user) => {
-        if (user !== socket) {
-          user.emit("chat message", msg);
-        }
-      });
-    });
-
-    socket.on("disconnect", () => {
-      users = users.filter((user) => user !== socket);
-      console.log("User disconnected");
-      users.forEach((user) => {
-        user.emit("user disconnected", {
-          msg: "The other user has disconnected.",
-        });
-      });
-    });
-  } else {
-    socket.emit("user connected", {
-      msg: "Chat room is full. Please try again later.",
-    });
-    socket.disconnect();
+  if (!userLimitSet) {
+    socket.emit("request user limit");
   }
+
+  socket.on("set user limit", (limit) => {
+    if (!userLimitSet) {
+      userLimit = limit;
+      userLimitSet = true;
+      socket.emit("user limit set", userLimit);
+      socket.broadcast.emit("user limit set", userLimit);
+    }
+  });
+
+  socket.on("join chat", (name) => {
+    if (users.length < userLimit) {
+      const user = { id: socket.id, name: name };
+      users.push(user);
+      socket.emit("user connected", {
+        msg: `You are connected as ${name}. Waiting for other users...`,
+      });
+
+      if (users.length === userLimit) {
+        users.forEach((user) => {
+          io.to(user.id).emit("user connected", {
+            msg: `${user.name} connected. You can now chat.`,
+          });
+        });
+      }
+
+      socket.on("chat message", (msg) => {
+        users.forEach((user) => {
+          if (user.id !== socket.id) {
+            io.to(user.id).emit("chat message", {
+              name: msg.name,
+              data: msg.data,
+              type: msg.type,
+            });
+          }
+        });
+      });
+
+      socket.on("disconnect", () => {
+        users = users.filter((user) => user.id !== socket.id);
+        users.forEach((user) => {
+          io.to(user.id).emit("user disconnected", {
+            msg: `${name} has disconnected.`,
+          });
+        });
+      });
+    } else {
+      socket.emit("user connected", {
+        msg: "Chat room is full. Please try again later.",
+      });
+      socket.disconnect();
+    }
+  });
 });
 
 server.listen(PORT, () => {
